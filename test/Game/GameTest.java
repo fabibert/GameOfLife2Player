a01ebdfe15339f14cf197877569708e236825a95
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2022-12-27 08:53:06 +0700

    Adding maven and checking importing mockito works

--- test/Game/GameTest.java
+++ test/Game/GameTest.java
@@ -1,10 +1,15 @@
 package Game;
 
+import State.State;
+import State.StateBuilder;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
+    @Mock StateBuilder<State> stateBuilder;
 
     @Test
     public void testInitInitializesTheStateWithTwoDifferentNames(){
