3b387d3e7234f36c36443171ea995d89275aa17c
fabibert <76185888+fabibert@users.noreply.github.com>
2022-12-28 08:25:17 +0100

    board implementation

--- src/State/GolBoardImpl.java
+++ src/State/GolBoardImpl.java
@@ -1,6 +1,24 @@
 package State;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public class GolBoardImpl extends GolBoard {
+
+    GolCell[][] board;
+
+    public GolBoardImpl(){
+        board = new GolCell[10][10];
+        for (GolCell[] row: board)
+            Arrays.fill(row, new GolCell(Optional.empty()));
+    }
+
+    public void setCell(int x,int y, String name) {
+        board[x][y] = new GolCell(Optional.of(new Player(name)));
+    }
+    public GolCell getCell(int x, int y) {
+        return board[x][y];
+    }
 
     @Override
     public GolCell[][] getArray(){
@@ -11,3 +29,12 @@ public class GolBoardImpl extends GolBoard {
     public void setArray(GolCell[][] array){
     }
 }
+
+class MyLaunch {
+    public static void main(String[] args){
+        GolBoardImpl board = new GolBoardImpl();
+        board.setCell(1,1, "Fabio");
+        System.out.println(board.getCell(1,1));
+    }
+}
+
