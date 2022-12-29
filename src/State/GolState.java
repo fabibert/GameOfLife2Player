1da898e77313464ec3770ad6f641d31939df3b07
fabibert <76185888+fabibert@users.noreply.github.com>
2022-12-29 17:12:00 +0100

    state changes

--- src/State/GolState.java
+++ src/State/GolState.java
@@ -2,10 +2,15 @@ package State;
 
 import java.util.List;
 
-public interface GolState extends State {
+public interface GolState extends State, Observable {
     List<Player> getPlayers();
     GolBoard getBoard();
+
+    void setBoard(GolBoard board);
+
     Player getCurrentPlayer();
     int getNumberOfGenerations();
     void increaseNumberOfGenerations();
+
+
 }
--- src/State/GolStateImpl.java
+++ src/State/GolStateImpl.java
@@ -1,12 +1,17 @@
 package State;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class GolStateImpl implements GolState{
 
     private final List<Player> players;
+
+    private List<Observer> observers;
     private int numberOfGenerations = 0;
     private GolBoard board;
+
 
 
     public GolStateImpl(String player1Name, String player2Name, GolBoard board) {
@@ -14,6 +19,7 @@ public class GolStateImpl implements GolState{
         Player player2 = new Player(player2Name);
         players = List.of(player1, player2);
         this.board = board;
+        observers = new ArrayList<Observer>();
     }
 
     @Override
@@ -24,6 +30,12 @@ public class GolStateImpl implements GolState{
     @Override
     public GolBoard getBoard() {
         return board;
+    }
+
+    @Override
+    public void setBoard(GolBoard board) {
+        this.board = board;
+        updateObservers(new EncapsulatedGolState(Map.of(), board,1, getCurrentPlayer()));
     }
 
     @Override
@@ -39,5 +51,25 @@ public class GolStateImpl implements GolState{
     @Override
     public void increaseNumberOfGenerations() {
         numberOfGenerations++;
+    }
+
+
+
+    @Override
+    public void updateObservers(EncapsulatedGolState state) {
+        for(Observer observer: observers){
+            observer.recieveGolStateEncapsulated(state);
+        }
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+
+    }
+
+    @Override
+    public void addObserver(Observer observer) {
+        observers.add(observer);
     }
 }
