8c1784a7127bd79b609dc672f98e57a2b00a1f78
fabibert <76185888+fabibert@users.noreply.github.com>
2022-12-30 12:40:45 +0100

    testing in new thread separately

--- test/UI/GolUIImplTest.java
+++ test/UI/GolUIImplTest.java
@@ -14,7 +14,10 @@ public class GolUIImplTest extends Application {
 
     GolUI ui;
     EncapsulatedGolState state;
+    EncapsulatedGolState state2;
     GolGame game;
+    private GolBoardImpl board1;
+
     public static void main(String[] args) {
         GolUIImplTest.launch();
     }
@@ -25,21 +28,37 @@ public class GolUIImplTest extends Application {
         Player player1 = new Player("Fabio");
         Player player2 = new Player("Joe");
         GolBoardImpl board = new GolBoardImpl(10, 10);
-        state = new EncapsulatedGolState(Map.of(player1, 4, player2, 3), board, 3, player1);
-
-        testRecieveGolStateEncapsulated();
-
         board.setCellToPlayer(1,1, player1.playerName());
-        board.setCellToPlayer(3,3, player1.playerName());
-        board.setCellToPlayer(5,4, player2.playerName());
+
+        board1 = new GolBoardImpl(10, 10);
+        board1.setCellToPlayer(1,1, player1.playerName());
+
         state = new EncapsulatedGolState(Map.of(player1, 4, player2, 3), board, 3, player1);
 
+        board1.setCellToPlayer(3,3, player1.playerName());
+        board1.setCellToPlayer(5,4, player2.playerName());
+
+        state2 = new EncapsulatedGolState(Map.of(player1, 4, player2, 3), board1, 3, player1);
         testRecieveGolStateEncapsulated();
     }
 
     @Test
     void testRecieveGolStateEncapsulated() {
-        new Thread(() -> ui.recieveGolStateEncapsulated(state)).start();
+
+        new Thread(() -> {
+            ui.recieveGolStateEncapsulated(state);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            ui.recieveGolStateEncapsulated(state2);
+            Coordinates coord = ui.requestPlayerCellCreation();
+            board1.setCellToPlayer(coord.x(), coord.y(), "Fabio");
+            ui.recieveGolStateEncapsulated(state2);
+            System.out.println(coord.x() + " " + coord.y());
+        }).start();
+
     }
 
     @Test
