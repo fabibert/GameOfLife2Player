26cb5d1630a790c402849796e801e22368ee6b46
fabibert <76185888+fabibert@users.noreply.github.com>
2022-12-31 16:39:58 +0100

    refactor players to instance variables to reduce player passing

--- src/Game/RegenerationImpl.java
+++ src/Game/RegenerationImpl.java
@@ -8,6 +8,10 @@ public class RegenerationImpl implements Regeneration {
     int neighbours1 = 0;
     int neighbours2 = 0;
 
+    Player player1;
+    Player player2;
+
+
     public RegenerationImpl(GolState golState) {
         this.golState = golState;
     }
@@ -16,44 +20,44 @@ public class RegenerationImpl implements Regeneration {
     public GolBoard regenerationBoard() {
         GolBoard oldBoard = golState.getBoard();
         GolBoard newBoard = new GolBoardImpl(oldBoard.getBoardHeight(), oldBoard.getBoardWidth());
-        Player player1 = golState.getPlayers().get(0);
-        Player player2 = golState.getPlayers().get(1);
+        this.player1 = golState.getPlayers().get(0);
+        this.player2 = golState.getPlayers().get(1);
         for (int y = 0; y < oldBoard.getBoardHeight(); y++) {
             for (int x = 0; x < oldBoard.getBoardWidth(); x++) {
                 Coordinates coordinates = new Coordinates(x, y);
-                cellCreationRules(oldBoard, newBoard, player1, player2, coordinates);
+                cellCreationRules(oldBoard, newBoard, coordinates);
             }
         }
         return newBoard;
     }
 
-    private void cellCreationRules(GolBoard oldBoard, GolBoard newBoard, Player player1, Player player2, Coordinates coordinates) {
+    private void cellCreationRules(GolBoard oldBoard, GolBoard newBoard, Coordinates coordinates) {
         GolCell golCell = oldBoard.getCell(coordinates);
         if (golCell.isAlive()) {
             Player currentOccupant = golCell.getPlayer();
-            if (getNumberOfAliveNeighbours(coordinates, player1, player2) < 2)
+            if (getNumberOfAliveNeighbours(coordinates) < 2)
                 newBoard.setCellEmpty(coordinates);
-            else if (getNumberOfAliveNeighbours(coordinates, player1, player2) == 2)
+            else if (getNumberOfAliveNeighbours(coordinates) == 2)
                 newBoard.setCellToPlayer(coordinates, currentOccupant);
-            else if(getNumberOfAliveNeighbours(coordinates, player1, player2) == 3)
-                if(this.neighbours1 > neighbours2)
-                    newBoard.setCellToPlayer(coordinates, player1);
+            else if(getNumberOfAliveNeighbours(coordinates) == 3)
+                if(this.neighbours1 > this.neighbours2)
+                    newBoard.setCellToPlayer(coordinates, this.player1);
                 else
-                    newBoard.setCellToPlayer(coordinates, player2);
-            else if (getNumberOfAliveNeighbours(coordinates, player1, player2) > 3)
+                    newBoard.setCellToPlayer(coordinates, this.player2);
+            else if (getNumberOfAliveNeighbours(coordinates) > 3)
                 newBoard.setCellEmpty(coordinates);
         }
         if (!golCell.isAlive()) {
-            if (getNumberOfAliveNeighbours(coordinates, player1, player2) == 3)
-                if(this.neighbours1 > neighbours2)
-                    newBoard.setCellToPlayer(coordinates, player1);
+            if (getNumberOfAliveNeighbours(coordinates) == 3)
+                if(this.neighbours1 > this.neighbours2)
+                    newBoard.setCellToPlayer(coordinates, this.player1);
                 else
-                    newBoard.setCellToPlayer(coordinates, player2);
+                    newBoard.setCellToPlayer(coordinates, this.player2);
         }
     }
 
 
-    private int getNumberOfAliveNeighbours(Coordinates coordinates, Player player1, Player player2) {
+    private int getNumberOfAliveNeighbours(Coordinates coordinates) {
         this.neighbours1 = 0;
         this.neighbours2 = 0;
 
@@ -63,8 +67,8 @@ public class RegenerationImpl implements Regeneration {
         for(int j = y-1; j<=y+1; j++){
             for(int i= x-1; i<= x+1; i++){
                 if(!(j==y && i==x)){
-                    this.neighbours1 += checkAlive(i,j,player1);
-                    this.neighbours2 += checkAlive(i,j,player2);
+                    this.neighbours1 += checkAlive(i,j,this.player1);
+                    this.neighbours2 += checkAlive(i,j,this.player2);
                 }
             }
         }
