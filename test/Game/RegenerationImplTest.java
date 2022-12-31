323f3a5e22d6b40aff46a06d532830d7c7d57061
fabibert <76185888+fabibert@users.noreply.github.com>
2022-12-31 18:22:11 +0100

    testing Regeneration works

--- test/Game/RegenerationImplTest.java
+++ test/Game/RegenerationImplTest.java
@@ -3,34 +3,107 @@ package Game;
 import UI.Coordinates;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import state.*;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.MockitoAnnotations.openMocks;
 
 class RegenerationImplTest {
 
-    @Mock
-    GolState state;
-    @Mock
-    GolBoard board;
     Coordinates coordinates;
     Player player1;
     Player player2;
     GolCell cell;
-    Verifier verifier;
+    Regeneration regeneration;
+
+    GolBoardImpl testInitialBoard;
+
+    GolBoardImpl testResultBoard;
+    GolState state;
+
+
 
     @BeforeEach
     public void setUp(){
         openMocks(this);
-        player1 = new Player("NAME_1");
-        player2 = new Player("NAME_2");
-        GolBoardImpl board = new GolBoardImpl(10,10);
-        board.setCellToPlayer(new Coordinates(8,8), player1);
+        player1 = new Player("Red");
+        player2 = new Player("Blue");
+
+        this.testInitialBoard = new GolBoardImpl(10,10);
+
+        setupTestInitialBoard(testInitialBoard);
+
+        this.state = new GolStateImpl(player1, player2, testInitialBoard);
+        regeneration = new RegenerationImpl(state);
+
+        this.testResultBoard = new GolBoardImpl(10,10);
+
+        setupTestResultBoard(testResultBoard);
     }
 
     @Test
-    void regenerationBoard() {
+    void testRegenerationBoard() {
+        this.state.setBoard(regeneration.regenerationBoard());
+        assertTrue(compareBoards());
+    }
 
+
+    boolean compareBoards(){
+        for (int y = 0; y < state.getBoard().getBoardHeight(); y++) {
+            for (int x = 0; x < state.getBoard().getBoardWidth(); x++) {
+                Coordinates coordinates = new Coordinates(x, y);
+                if(!state.getBoard().getCell(coordinates).equals(this.testResultBoard.getCell(coordinates)))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private void setupTestInitialBoard(GolBoardImpl testInitialBoard) {
+        testInitialBoard.setCellToPlayer(new Coordinates(1,1), player1);
+        testInitialBoard.setCellToPlayer(new Coordinates(2,1), player2);
+        testInitialBoard.setCellToPlayer(new Coordinates(3,1), player1);
+
+        testInitialBoard.setCellToPlayer(new Coordinates(6,1), player2);
+        testInitialBoard.setCellToPlayer(new Coordinates(8,1), player1);
+        testInitialBoard.setCellToPlayer(new Coordinates(7,3), player2);
+
+        testInitialBoard.setCellToPlayer(new Coordinates(7,7), player2);
+        testInitialBoard.setCellToPlayer(new Coordinates(8,7), player2);
+        testInitialBoard.setCellToPlayer(new Coordinates(7,8), player2);
+        testInitialBoard.setCellToPlayer(new Coordinates(8,8), player2);
+
+
+        testInitialBoard.setCellToPlayer(new Coordinates(0,7), player1);
+        testInitialBoard.setCellToPlayer(new Coordinates(1,7), player1);
+        testInitialBoard.setCellToPlayer(new Coordinates(2,7), player1);
+
+        testInitialBoard.setCellToPlayer(new Coordinates(0,8), player2);
+        testInitialBoard.setCellToPlayer(new Coordinates(1,8), player2);
+        testInitialBoard.setCellToPlayer(new Coordinates(2,8), player2);
+
+        testInitialBoard.setCellToPlayer(new Coordinates(0,9), player1);
+        testInitialBoard.setCellToPlayer(new Coordinates(1,9), player1);
+        testInitialBoard.setCellToPlayer(new Coordinates(2,9), player1);
+    }
+
+    private void setupTestResultBoard(GolBoardImpl testResultBoard) {
+        testResultBoard.setCellToPlayer(new Coordinates(2,0), player1);
+        testResultBoard.setCellToPlayer(new Coordinates(2,1), player2);
+        testResultBoard.setCellToPlayer(new Coordinates(2,2), player1);
+
+        testResultBoard.setCellToPlayer(new Coordinates(7,2), player2);
+
+        testResultBoard.setCellToPlayer(new Coordinates(1,6), player1);
+        testResultBoard.setCellToPlayer(new Coordinates(0,7), player2);
+        testResultBoard.setCellToPlayer(new Coordinates(2,7), player2);
+        testResultBoard.setCellToPlayer(new Coordinates(3,8), player1);
+        testResultBoard.setCellToPlayer(new Coordinates(0,9), player2);
+        testResultBoard.setCellToPlayer(new Coordinates(2,9), player2);
+
+        testResultBoard.setCellToPlayer(new Coordinates(7,7), player2);
+        testResultBoard.setCellToPlayer(new Coordinates(8,7), player2);
+        testResultBoard.setCellToPlayer(new Coordinates(7,8), player2);
+        testResultBoard.setCellToPlayer(new Coordinates(8,8), player2);
     }
 }
