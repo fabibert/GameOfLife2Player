99c6bbe55ab9bad7560b014209416a9da6e3ddaa
fabibert <76185888+fabibert@users.noreply.github.com>
2023-01-02 03:11:27 +0100

    test state builder

---
+++ test/state/GolStateBuilderTest.java
@@ -0,0 +1,55 @@
+package state;
+
+import UI.Coordinates;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+class GolStateBuilderTest {
+    private GolStateBuilder builder;
+    private GolState state;
+    private String player1Name;
+    private String player2Name;
+    private Player player1;
+    private Player player2;
+    private GolBoardImpl golBoard;
+
+    @BeforeEach
+    public void setUp(){
+        openMocks(this);
+        player1Name = "Joe";
+        player2Name = "Fabio";
+        player1 = new Player(player1Name);
+        player2 = new Player(player2Name);
+        golBoard = new GolBoardImpl(10, 10);
+        state = new GolStateImpl(player1, player2, golBoard);
+        builder = new GolStateBuilder();
+    }
+
+    @Test
+    public void testPlayerStoringAndAlphabeticalOrder(){
+        GolState state = builder.createState(player1Name,player2Name);
+        assertTrue(state.getPlayers().get(0).playerName().equals(player2Name));
+    }
+
+    @Test
+    public void testStateInitWinner(){
+        GolState state = builder.createState(player1Name,player2Name);
+        assertFalse(state.checkForWinner());
+    }
+
+    @Test
+    public void testStateInitLeadingPlayerAlphabeticallyFirst(){
+        GolState state = builder.createState(player1Name,player2Name);
+        assertFalse(state.getLeadingPlayer().equals(player2));
+    }
+
+    @Test
+    public void testStateInitBoardPattern(){
+        GolState state = builder.createState(player1Name,player2Name);
+        assertTrue(state.getBoard().getCell(new Coordinates(2,3)).getPlayer().equals(player1));
+    }
+}
